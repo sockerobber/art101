43454100cecc1ea1e1f2a34096bfaac187ba9d31
sockerobber <129994400+sockerobber@users.noreply.github.com>
2023-04-25 13:17:17 -0700

    lab5 changes

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Learning Data Types and Variables 
+// Author: Brandon Vindel <bvindelf@ucsc.edu>
+// Date: April 25, 2023
 
-// Constants
+// Define Variables
+make = "Honda";
+model = "Accord";
+color = "Grey";
+year = "2010";
 
-// Functions
+// calculate
+age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
