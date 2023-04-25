0c3693e71b6fbce845b746f731a4ff67b8cfa964
sockerobber <129994400+sockerobber@users.noreply.github.com>
2023-04-24 20:55:07 -0700

    finished product

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - An implementation of Hangman
+// Author: Brandon Vindel (bvindelf@ucsc.edu)
+// Date: April 24, 2023
 
-// Constants
+// Constants 
+
+//Print a tree
+//Ask the user to type in a word or ask the computer to generate a word
+  //If generated, ask the user to choose the number of letters in the word
+//Draw a dashed line
+  //Print '_' per letter in the word
+//Ask the user to input a letter (guess)
+  //If the letter is wrong, Draw a piece of the man being hanged from the tree
+    //Starting with his head going down (head, neck, left arm, right arm)
+    //Show the wrong letter to the side of the tree to keep mark of the incorrect letters
+//If the letter is in the word, show the letter on top of a dashed line for everytime the letter comes out in the word
+//Check for win/lose condition
+  //If the word is found, the user wins 
+  //If the man is hanged(ran out of lives), the user loses
+//Ask user if they'd like to play again
+
 
 // Functions
 
